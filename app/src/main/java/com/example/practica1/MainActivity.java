283725f348ca57e66a1ddb7c1a0367110ce98316
Ramiro4903 <ramiro.ramirez.guzman@gmail.com>
2025-08-21 08:02:51 -0600

    Se agrego la transicion de pantalla.

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.practica1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,12 +14,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Thread thread = new Thread(){
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(3000);
+                }
+
+                catch (InterruptedException e){e.printStackTrace();}
+                finally {
+                    Intent intent = new Intent(MainActivity.this,inicio.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }; thread.start();
+
     }
 }
---
+++ app/src/main/java/com/example/practica1/inicio.java
@@ -0,0 +1,18 @@
+package com.example.practica1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class inicio extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_inicio);
+    }
+}
+
