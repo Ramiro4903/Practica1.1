489386233785379b4954dc24bf14b8789ddf529d
Ramiro4903 <ramiro.ramirez.guzman@gmail.com>
2025-08-28 08:30:41 -0600

    Agrege 2 botones y cambio de imagnes

--- app/src/main/java/com/example/practica1/inicio.java
+++ app/src/main/java/com/example/practica1/inicio.java
@@ -1,6 +1,11 @@
 package com.example.practica1;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +13,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class inicio extends AppCompatActivity {
+public class inicio extends AppCompatActivity implements View.OnClickListener {
+    private Button b;
+    private Button b2;
+    private TextView t;
+
+    private ImageView i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_inicio);
+
+        b = (Button) findViewById(R.id.button);
+        t = (TextView) findViewById(R.id.textView2);
+        i = (ImageView) findViewById(R.id.imageView2);
+        b2 = (Button) findViewById(R.id.button2);
+
+        b.setOnClickListener(this);
+        b2.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button){
+            t.setText("Mario ya chambea");
+            i.setImageResource(R.drawable.ya_chambea);
+        }
+        else if(v.getId() == R.id.button2){
+            t.setText("Mario se dio de baja");
+            i.setImageResource(R.drawable.baja);
+        }
+    }
+
 }
 
